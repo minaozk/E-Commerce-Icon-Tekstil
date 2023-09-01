9e79509005424dd5391ca765c6986a9b319bc1d3
mina <minaozk66@gmail.com>
2023-09-01 19:04:50 +0300

    Add project files.

---
+++ IconTekstil.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IconTekstil.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
